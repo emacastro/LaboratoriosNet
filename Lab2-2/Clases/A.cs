e38f95ca47cc45508716709cbd6bf253f8e97cd9
emacastro <crzemanuelc@gmail.com>
2015-05-03 20:29:51 -0300

    Lab2-2 Hasta ejercicio 3

---
+++ Lab2-2/Clases/A.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class A
+    {
+        string _nombreinstancia;
+        public string NombreInstancia
+        {
+            get { return _nombreinstancia; }
+            set { _nombreinstancia = value; }
+        }
+
+        public A()
+        {
+            NombreInstancia = "Instancia sin nombre";
+        }
+
+        public A(string nombre)
+        {
+            NombreInstancia = nombre;
+        }
+
+        public void MostrarNombre()
+        {
+            Console.WriteLine(NombreInstancia);
+        }
+
+        public void M1()
+        {
+            Console.WriteLine("El metodo M1 fue invocado.");
+        }
+        public void M2()
+        {
+            Console.WriteLine("El metodo M2 fue invocado.");
+        }
+        public void M3()
+        {
+            Console.WriteLine("El metodo M3 fue invocado.");
+        }
+    }
+}
---
+++ Lab2-2/LabClases1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clases;
+
+namespace LabClases1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A objetoA = new A();
+            B objetoB = new B();
+            objetoA.MostrarNombre();
+            objetoA.M1();
+            objetoA.M2();
+            objetoA.M3();
+            objetoB.MostrarNombre();
+            objetoB.M1();
+            objetoB.M2();
+            objetoB.M3();
+            objetoB.M4();
+            Console.ReadKey();
+        }
+    }
+}
