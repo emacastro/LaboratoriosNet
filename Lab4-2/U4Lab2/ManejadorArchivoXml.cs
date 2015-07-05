5de587882ee974b50fbc444a22a24de6d583fe50
emacastro <crzemanuelc@gmail.com>
2015-07-05 18:44:38 -0300

    Lab4-2 Finalizado

---
+++ Lab4-2/U4Lab2/ManejadorArchivoXml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace U4Lab2
+{
+    class ManejadorArchivoXml : ManejadorArchivo
+    {
+        protected DataSet ds;
+        public override System.Data.DataTable getTabla()
+        {
+            this.ds = new DataSet();
+            this.ds.ReadXml("agenda.xml");
+            return this.ds.Tables["contactos"];
+        }
+        public override void aplicaCambios()
+        {
+            this.ds.WriteXml("agenda.xml");
+            this.ds.WriteXml("agendaconschema.xml", XmlWriteMode.WriteSchema);
+        }
+    }
+}
---
+++ Lab4-2/U4Lab2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U4Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManejadorArchivo manejadorArchivo;
+            Console.WriteLine("Elija el modo:");
+            Console.WriteLine("1- TXT");
+            Console.WriteLine("2- XML");
+            if (Console.ReadLine() == "2")
+            {
+                manejadorArchivo = new ManejadorArchivoTxt();
+            }
+            else
+            {
+                manejadorArchivo = new ManejadorArchivoXml();
+            }
+            manejadorArchivo.listar();
+            menu(manejadorArchivo);
+        }
+
+        public static void menu(ManejadorArchivo manejadorArchivo)
+        {
+            string rta="";
+            do
+            {
+                Console.WriteLine("1 - Listar");
+                Console.WriteLine("2 - Agregar");
+                Console.WriteLine("3 - Modificar");
+                Console.WriteLine("4 - Eliminar");
+                Console.WriteLine("5 - Guardar Cambios");
+                Console.WriteLine("6 - Salir");
+                rta = Console.ReadLine();
+                switch (rta)
+                {
+                    case "1":
+                        manejadorArchivo.listar();
+                        break;
+                    case "2":
+                        manejadorArchivo.nuevaFila();
+                        break;
+                    case "3":
+                        manejadorArchivo.editarFila();
+                        break;
+                    case "4":
+                        manejadorArchivo.eliminarFila();
+                        break;
+                    case "5":
+                        manejadorArchivo.aplicaCambios();
+                        break;
+                    default:
+                        break;
+                }
+            } while (rta != "6");
+        }
+    }
+
+ }
