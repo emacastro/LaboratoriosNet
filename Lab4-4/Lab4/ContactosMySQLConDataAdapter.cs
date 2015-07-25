eb011b6a52206446d3e06a628f2e046190096a3c
emacastro <crzemanuelc@gmail.com>
2015-07-25 16:20:48 -0300

    Lab4-4

---
+++ Lab4-4/Lab4/ContactosMySQLConDataAdapter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class ContactosMySQLConDataAdapter:Contactos
+    {
+        public string connectionString { get { return "server=isi;database=net;uid=net;pwd=net"; } }
+        public ContactosMySQLConDataAdapter():base()
+        {
+            //this.
+        }
+    }
+}
